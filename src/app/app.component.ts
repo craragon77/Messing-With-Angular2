5edac9cc137051950b9c4c9bfc6d010f4f82b586
craragon77 <aragonchristopherray@gmail.com>
2024-07-15 22:19:16 -0400

    change of plans, lets fetch some dogs to learn what i'm doing lmfao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,20 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'vt-hiking-trails';
   enteredLocation = new FormControl('');
+  localImg = new String;
+  searchComplete = false;
+  async fetchDogs(): Promise<String | null>{
+    const data = await fetch('https://dog.ceo/api/breeds/image/random')
+    return await data.json()
+  }
   searchLocation:Function = () => {
     console.log('testLocal', this.enteredLocation);
+    this.fetchDogs()
+    .then((res:any) => {
+      console.log('res', res);
+      this.localImg = res.message;
+      this.searchComplete = true;
+    })
+    .catch((err) => console.log('err', err));
   };
-
 }
